225deec388eddca16811fbe29a919c0c2ca74a41
AJI-Hsu <mrhsu950325@gmail.com>
2024-11-26 23:40:42 +0800

    GramSchmidtProcess

--- FunctionSet.java
+++ FunctionSet.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// limit number of rows cannot exceed 10000
 public class FunctionSet {
-    private FunctionSet() {
+    static final double doubleError = 0.001;
+    static final int limitRun = 10000;
+
+    private FunctionSet() { // cannot create an object
     }
 
     public static List<int[]> run(int[] x1, int[] x2, int[] y) {
@@ -15,20 +19,30 @@ public class FunctionSet {
         return res;
     }
 
-    private static int[][] transpose(int[][] arr) {
+    private static double[][] transpose(double[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
-        int[][] trans = new int[n][m];
+        double[][] trans = new double[n][m];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 trans[j][i] = arr[i][j];
             }
         }
-        return null;
+        return trans;
     }
 
-    private static int[][] multiply(int[][] arr1, int[][] arr2) {
+    private static double[][] transpose(double[] vector) { // 1D transpose
+        double[][] trans = new double[vector.length][1];
+
+        for (int i = 0; i < vector.length; i++) {
+            trans[i][0] = vector[i];
+        }
+
+        return trans;
+    }
+
+    private static double[][] multiply(double[][] arr1, double[][] arr2) {
         int m1 = arr1.length;
         int n1 = arr1[0].length;
         int m2 = arr2.length;
@@ -39,7 +53,7 @@ public class FunctionSet {
             return null;
         }
 
-        int[][] result = new int[m1][n2];
+        double[][] result = new double[m1][n2];
 
         for (int i = 0; i < m1; i++) {
             for (int j = 0; j < n2; j++) {
@@ -52,12 +66,98 @@ public class FunctionSet {
         return result;
     }
 
-    private int[][] inverse(int[][] arr) {
+    private static double[] minus(double[] a, double[] b) {
+        if (a.length != b.length) {
+            System.err.println("Error: a.length != b.length at function : minus");
+            System.exit(1);
+        }
+        double[] result = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[i] - b[i];
+        }
+        return result;
+    }
+
+    private static boolean isZeroVector(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] != 0) return false;
+        }
+        return true;
+    }
+
+    /* test passed (1) */
+    private static boolean isZeroVector(double[] v) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > doubleError) return false;
+        }
+        return true;
+    }
+
+    /* test passed */
+    private static double[][] GramSchmidtProcess(double[][] A) { // one row is one vector
+        int m = A.length; // row number
+        int n = A[0].length; // Rn
+        int idx = 0;
+        ArrayList<double[]> Q = new ArrayList<>();
+        double[] v;
+        double len;
+
+        // first step
+        v = A[idx++]; // error vector (let e1 = v1)
+        len= 0; // length of vector
+        for (double i : v) {
+            len += i * i;
+        }
+
+        len = Math.sqrt(len);
+        for (int i = 0; i < n; i++) { // normalization
+            v[i] /= len;
+        }
+
+        // step iteration
+        while (!isZeroVector(v)) {
+            Q.add(v); // add to Q
+
+            if (idx >= m) break;
+
+            // find error vector
+            v = A[idx++];
+            for (double[] q : Q) { // vk = sigma(k-1, 1)_qi^T vk qi
+                double t = 0;
+                for (int i = 0; i < q.length; i++) t += q[i] * v[i];
+                for (int i = 0; i < v.length; i++) v[i] -= t * q[i];
+            }
+
+            // normalization
+            len= 0; // length of vector
+            for (double i : v) {
+                len += i * i;
+            }
+
+            len = Math.sqrt(len);
+            for (int i = 0; i < n; i++) { // normalization
+                v[i] /= len;
+            }
+        }
+
+        double[][] result = new double[Q.size()][];
+        for (int i = 0; i < Q.size(); i++) {
+            result[i] = Q.get(i);
+        }
+        return result;
+    }
+
+    private static int[][] QRFactorization(int[][] A) { // int[0] = Q, int[1] = R
         // todo
         return null;
     }
 
-    private int[] findX(int[][] A, int[] b) { // (A^TA)^-1A^TB
+    private static int[] findX(int[][] A, int[] b) { // (A^TA)^-1A^TB or A=QR
+        // todo
+        return null;
+    }
+
+    private static int[] findError(int[][] A, int[] b) { // b - p = b - Ax
         // todo
         return null;
     }
--- Main.java
+++ Main.java
@@ -1,5 +1,76 @@
+import java.util.ArrayList;
+// test environment
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static boolean isZeroVector(double[] v) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > FunctionSet.doubleError) return false;
+        }
+        return true;
+    }
+
+    private static double[][] GramSchmidtProcess(double[][] A) { // one row is one vector
+        int m = A.length; // row number
+        int n = A[0].length; // Rn
+        int idx = 0;
+        ArrayList<double[]> Q = new ArrayList<>();
+        double[] v;
+        double len;
+
+        // first step
+        v = A[idx++]; // error vector (let e1 = v1)
+        len= 0; // length of vector
+        for (double i : v) {
+            len += i * i;
+        }
+
+        len = Math.sqrt(len);
+        for (int i = 0; i < n; i++) { // normalization
+            v[i] /= len;
+        }
+
+        // step iteration
+        while (!isZeroVector(v)) {
+            Q.add(v); // add to Q
+
+            if (idx >= m) break;
+
+            // find error vector
+            v = A[idx++];
+            for (double[] q : Q) { // vk = sigma(k-1, 1)_qi^T vk qi
+                double t = 0;
+                for (int i = 0; i < q.length; i++) t += q[i] * v[i];
+                for (int i = 0; i < v.length; i++) v[i] -= t * q[i];
+            }
+
+            // normalization
+            len= 0; // length of vector
+            for (double i : v) {
+                len += i * i;
+            }
+
+            len = Math.sqrt(len);
+            for (int i = 0; i < n; i++) { // normalization
+                v[i] /= len;
+            }
+        }
+
+        double[][] result = new double[Q.size()][];
+        for (int i = 0; i < Q.size(); i++) {
+            result[i] = Q.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] argv) {
+        double[][] arr =  {
+                { 1, -1, 0 },
+                { 1, 0, -2 },
+                { 3, -1, 3 }
+        };
+        arr = GramSchmidtProcess(arr);
+        for (double[] i : arr) {
+            for (double j : i) System.out.print(j + " ");
+            System.out.println();
+        }
     }
 }
