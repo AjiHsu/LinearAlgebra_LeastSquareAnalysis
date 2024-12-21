28339e23caf2e4b3a806812be827e5c692f763bf
AJI-Hsu <mrhsu950325@gmail.com>
2024-12-21 18:08:53 +0800

    time limit=1s test

--- Main.java
+++ Main.java
@@ -1,22 +1,45 @@
+import java.util.Date;
+
 // test environment
 public class Main {
     // test
     public static void main(String[] argv) {
 
         // input data must be linear independent : y(x1i, x2i) y(x1j, x2j) for all i != j : !(x1i == x1j && x2i == x2j) is always true
-        double[] x1 = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
-                11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21};
-        double[] x2 = {0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5,
-                10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20};
-        double[] y = {-185.24, 47.8, 86.1, 124.3, 162.5, 200.8, 239.0, 277.3, 315.5,
-                353.7, 391.9, 430.2, 468.5, 506.7, 545.0, 583.2, 621.4,
-                659.7, 697.9, 736.1, 1500.0};
+        double[] x1;
 
-        double[] x = FunctionSet.run(x1, x2, y);
+        double[] x2;
 
-        for (int i = 0; i < x.length; i++) {
-            System.out.print(x[i] + " ");
+        double[] y;
+
+        int maxSize = 1100000;
+        int timeLimit = 10000;
+        x1 = new double[maxSize];
+        x2 = new double[maxSize];
+        y = new double[maxSize];
+        for (int i = 0; i < maxSize; i++) {
+            x1[i] = i;
+            x2[i] = i * 0.1289;
+            y[i] = i / 20d + Math.sqrt(i);
         }
-        System.out.println();
+
+        Date start = new Date();
+        double[] x = FunctionSet.run(x1, x2, y);
+        Date end = new Date();
+        if (start.getTime() - end.getTime() > timeLimit) System.err.println("Time limit exceeded");
+        else System.out.println("time elapsed:" + (end.getTime() - start.getTime()) + "ms");
+
+        System.out.println("the equation is: ");
+        System.out.println("y = " + x[0] + "x1 + " + x[1] + "x2 = " + x[2]);
+
+//        double[] y2 = new double[y.length];
+//        for (int i = 0; i < y.length; i++) {
+//            y2[i] = x1[i] * x[0] + x2[i] * x[1] + x[2];
+//        }
+//
+//        System.out.println("y vs y' : ");
+//        for (int i = 0; i < y2.length; i++) {
+//            System.out.println(y[i] + " " + y2[i]);
+//        }
     }
 }
